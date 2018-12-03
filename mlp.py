ff2336e5d3b69cdcfe3510de7cab3f712078f027
unknown <paulmc@uw.ed>
2018-12-03 13:48:10 -0800

    trying to get mlp to work better (undersampling)

--- mlp.py
+++ mlp.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from numpy import nan
 from numpy import array
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
 from keras.models import Sequential
 from keras.layers import Dense
 
@@ -31,9 +31,8 @@ detector2Path = preTrainedPath + "haarcascade_frontalface_default.xml"
 detector3Path = preTrainedPath + "mmod_human_face_detector.dat"
 shapePredPath = preTrainedPath + "shape_predictor_68_face_landmarks.dat"	
 
-
-
 def extract_features_labels(tr):
+	print("extracing features, labels")
 
 	# normalize
 	tr[:,EAR] = scipy.stats.zscore(tr[:,EAR])
@@ -44,26 +43,24 @@ def extract_features_labels(tr):
 
 	# Mapping frames to their classification
 	tr_vectors = []
-	numblinks = 0
+
+	num_blinks = 0
 
 	# extract blinks 
 	for i in range(F_VECTOR_LENGTH,len(tr)-F_VECTOR_LENGTH):
 		if(tr[i][LABELED_BLINK] == 1):
 			blinkMap[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1] = 1
-			# BALANCING DATA
-			for z in range(100):
-				numblinks += 1
-				tr_vectors.append((tr[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1],1,i))
-			
+			tr_vectors.append((tr[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1],1,i))	
+			num_blinks += 1
+
 	# extract non blinks
 	for i in range(F_VECTOR_LENGTH,len(tr)-F_VECTOR_LENGTH):
 		if(blinkMap[i] == 0):
 			if(1 not in blinkMap[i - F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1]):
 				tr_vectors.append((tr[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1],0,i))
-
-
+			
 	tr_vectors = sorted(tr_vectors,key=lambda x: x[2])
-	
+
 	X = []
 	y = []
 	for chunk, label, index in tr_vectors:
@@ -74,11 +71,7 @@ def extract_features_labels(tr):
 	X = np.asarray(X)
 	y = np.asarray(y)
 	
-	# pd.Series(blinkMap.flatten()).plot()
-	# pd.Series(y.flatten()).plot()
-	# plt.show()
-	
-	return np.asarray(X),np.asarray(y)
+	return np.hstack((X,y.reshape((-1,1))))
 
 def extract_features(tr):
 
@@ -99,7 +92,7 @@ def extract_features(tr):
 	y = (tr[:,LABELED_BLINK].astype(int))[F_VECTOR_LENGTH:len(tr)-F_VECTOR_LENGTH]
 	return X,y
 	
-def extract_features_chunked(tr):
+def extract_features_naive(tr):
 	# normalize
 	tr[:,0] = scipy.stats.zscore(tr[:,EAR])
 	tr[:,1] = scipy.stats.zscore(tr[:,GRADIENT])
@@ -107,7 +100,7 @@ def extract_features_chunked(tr):
 	tr_vectors = []
 	# extract features with step length
 	for i in range(F_VECTOR_LENGTH,len(tr)-F_VECTOR_LENGTH):
-			tr_vectors.append(tr[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1])
+			tr_vectors.append(tr[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1],tr[i][LABELED_BLINK])
 	
 	X = []
 	for chunk in tr_vectors:
@@ -127,6 +120,8 @@ def windowize_labels(labels):
 	return new_labels
 
 def test_model(fname):
+
+	print("testing model")
 	model = Sequential()
 	model.add(Dense(100, activation='relu', input_dim=14))
 	model.add(Dense(1))
@@ -164,7 +159,41 @@ def test_model(fname):
 
 	plt.show()
 	
-print("loading in ears")
+def under_sample_balance(extracted_data):
+	X,y = X_y(extracted_data)
+	
+	to_balance = np.hstack((X,y.reshape((-1,1))))
+
+	# balance
+	non_blinks = to_balance[to_balance[:,14] == 0]
+	blinks = to_balance[to_balance[:,14] == 1]
+	idx = np.random.randint(len(non_blinks),size=len(blinks))
+	non_blinks = non_blinks[idx]
+
+	to_balance = np.vstack((blinks,non_blinks))
+	X = to_balance[:,range(14)]
+	y = to_balance[:,14]
+	
+	return np.hstack((X,y.reshape((-1,1))))
+
+# pulls off the last column as labels, the rest as feature vectors
+def X_y(data):
+	n = data.shape[1]
+	X = data[:,range(n-1)]
+	y = data[:,n-1]
+	return X,y
+	
+# gives a test case
+def test_tr():
+	a = np.arange(300)
+	b = np.zeros(300)
+	b[4] = 1.0
+	tr = np.vstack((a,a*10,a*100,b)).T
+	
+	return tr
+	
+	
+# print("loading in ears")
 txt1 = csvPath + "planesweater1_ears.csv"
 ears = pd.read_csv(txt1,sep=',',header=None).values
 gradient = np.gradient(ears[:,EAR]).reshape((-1,1))
@@ -176,53 +205,58 @@ labels = pd.read_csv(txt2,sep=',',header=None).values
 raw = np.hstack((ears,labels))
 
 # remove nan entries
-raw = raw[~np.isnan(raw).any(axis=1)]
+tr = raw[~np.isnan(raw).any(axis=1)]
 
-print("extracting features")
+# ----- get x,y ---- 
 
-X,y = extract_features_labels(raw)
+# normalize
+tr[:,0] = scipy.stats.zscore(tr[:,EAR])
+tr[:,1] = scipy.stats.zscore(tr[:,GRADIENT])
+
+extracted_data = extract_features_labels(tr)
+sampled_data = under_sample_balance(extracted_data)
+
+np.random.shuffle(sampled_data)
+X,y = X_y(sampled_data)
+
+# -----------
 
 model = Sequential()
 model.add(Dense(100, activation='relu', input_dim=14))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
-#model.fit(X,y,epochs=50,verbose=1)
-#model.save_weights("D:\\blink-detection\\saved_models\\model2.hdf5")
-model.load_weights("D:\\blink-detection\\saved_models\\model2.hdf5")
-# now try on original video
+model.fit(X,y,epochs=300,verbose=0)
+model.save_weights("D:\\blink-detection\\saved_models\\model2.hdf5")
+# model.load_weights("D:\\blink-detection\\saved_models\\model2.hdf5")
 
-y_predict = (np.asarray(model.predict(X)) > 0.5).astype(int)
-# print(np.unique(y_predict))
-# print("--")
-# print(np.unique(y))
+y_predict = (model.predict(X) > 0.5).astype(int)
+pd.Series(y_predict.flatten()).plot()
+pd.Series(y.flatten()).plot()
+plt.show()
 
 recall = recall_score(y,y_predict)
 precision = precision_score(y,y_predict)
+accuracy = accuracy_score(y,y_predict)
 
+print("total blinks predicted: " + str(np.sum(y_predict)))
 print(recall)
 print(precision)
-
-pd.Series(y_predict.flatten()).plot()
-pd.Series(y.flatten()/4).plot()
-plt.show()	
-
-print(y_predict)
-
-test_model("planesweater1")
+print(accuracy)
 
 
-
-	
-#fname = "planesweater3"
-#test_model(fname)
-
-# fname = "planesweater1"
-# test_model(fname)
-
-# timestamps = (ears[F_VECTOR_LENGTH:len(ears)-F_VECTOR_LENGTH])[:,TIMESTAMP].reshape((-1,1))
+# STORAGE ---------------
 
 
-
+# Over sample:
+# balance_num = int(abs(len(tr_vectors)-num_blinks)/num_blinks)
+# n = len(tr_vectors)
+# new_rows = []
+# for i in range(n):
+	# chunk, label, index = tr_vectors[i]
+	# if(label == 1):
+		# for j in range(balance_num):
+			# new_rows.append(tr_vectors[i])
+# tr_vectors += new_rows
 
 
 
