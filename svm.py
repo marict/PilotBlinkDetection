3b7fa97449b78f0e699a6a00baf3c125c80f96b2
Paul Curry <paulmc@uw.edu>
2018-12-03 22:16:30 -0800

    svm and mlp work, probably going to try Adaboost next

--- svm.py
+++ svm.py
@@ -1,3 +1,4 @@
+# try using an svm for the series analysis
 import numpy as np
 import math 
 import scipy.stats
@@ -5,24 +6,22 @@ import pprint
 import os
 import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import uniform
 from sklearn import svm
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_validate, GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
 from sklearn.externals import joblib
 from sklearn.pipeline import make_pipeline
 from numpy import nan
 
-# Other features to consider:
-	# Derivative of EAR
-	# Euclidean distance between eyebrow and eye
-
-# SVM classifier
-# Data formats:
-# 	tr = [int timestamp,float ear,int labled_blinks]
-# 	classification = [int timestamp,float ear]
 
 TIMESTAMP = 0
 EAR = 1
-LABELED_BLINK = 2
+GRADIENT = 2
+LABELED_BLINK = 3
+F_VECTOR_LENGTH = 3
 
 basePath = os.path.dirname(os.path.realpath(__file__)) + "\\"
 vidPath = basePath + "vids\\"
@@ -32,234 +31,217 @@ preTrainedPath = basePath + "pre_trained_models\\"
 
 detector2Path = preTrainedPath + "haarcascade_frontalface_default.xml"
 detector3Path = preTrainedPath + "mmod_human_face_detector.dat"
-shapePredPath = preTrainedPath + "shape_predictor_68_face_landmarks.dat"
+shapePredPath = preTrainedPath + "shape_predictor_68_face_landmarks.dat"	
 
+# raw is the raw windows
+def extract_features_labels_true(raw, use_gradient = False):
+	blinkMap = np.zeros(len(raw))
+	true_values = []
+	label = raw.shape[1] - 1
+	
+	# mark off no 0 zones
+	for i in range(len(raw)):
+		if raw[i][label] == 1:
+			blinkMap[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1] = 1
+	
+	true_values = []
+	# remove non-blinks that are within the no 0 zones
+	for i in range(len(raw)):
+		if raw[i][label] == 0:
+			if(1 not in blinkMap[i - F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1]):
+				true_values.append(raw[i])
+		else:
+			true_values.append(raw[i])
+				
+	return np.asarray(true_values)
 
-class SVM:
-	tr_vectors = None
-	ts_vectors = None
-	internal_model = None
-	F_VECTOR_LENGTH = 3
+# extracts features in rolling window fashion
+def extract_features_labels_raw(ts,use_gradient = False):
+
+	# normalize
+	ts[:,EAR] = scipy.stats.zscore(ts[:,EAR])
+	ts[:,GRADIENT] = scipy.stats.zscore(ts[:,GRADIENT])
+	
+	tr_vectors = []
+	# extract features with step length
+	for i in range(F_VECTOR_LENGTH,len(ts)-F_VECTOR_LENGTH):
+			tr_vectors.append(ts[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1])
+		
+	X = []
+	for chunk in tr_vectors:
+		if(use_gradient):
+			X.append([x[EAR] for x in chunk] + [x[GRADIENT] for x in chunk])
+		else:
+			X.append([x[EAR] for x in chunk])
+	X = np.array(X)
+	
+	y = (ts[:,LABELED_BLINK].astype(int))[F_VECTOR_LENGTH:len(ts)-F_VECTOR_LENGTH]
+	return np.hstack((X,y.reshape((-1,1))))
+	
+def under_sample_balance(extracted_data):
+	X,y = X_y(extracted_data)
+	n = X.shape[1]
+	to_balance = np.hstack((X,y.reshape((-1,1))))
+
+	
+	# balance
+	non_blinks = to_balance[to_balance[:,n] == 0]
+	blinks = to_balance[to_balance[:,n] == 1]
+	idx = np.random.randint(len(non_blinks),size=len(blinks))
+	non_blinks = non_blinks[idx]
+
+	to_balance = np.vstack((blinks,non_blinks))
+	X = to_balance[:,range(n)]
+	y = to_balance[:,n]
+	
+	return np.hstack((X,y.reshape((-1,1))))
+
+# copy non blink number of blinks
+def over_sample_balance(extracted_data):
+	X,y = X_y(extracted_data)
+	to_balance = np.hstack((X,y.reshape((-1,1))))
+
+	# balance
+	non_blinks = to_balance[to_balance[:,14] == 0]
+	blinks = to_balance[to_balance[:,14] == 1]
+	
+	balance_num = int(len(non_blinks)/len(blinks))
+	to_balance = np.concatenate((blinks,non_blinks),axis=0)
+	
+	for i in range(balance_num):
+		to_balance = np.concatenate((to_balance,blinks),axis=0)
+	
+	X = to_balance[:,range(14)]
+	y = to_balance[:,14]
+	
+	return np.hstack((X,y.reshape((-1,1))))
+
+# pulls off the last column as labels, the rest as feature vectors
+def X_y(data):
+	n = data.shape[1]
+	X = data[:,range(n-1)]
+	y = data[:,n-1]
+	return X,y
+
+# cv	
+def cross_val(X,y):
 	pp = pprint.PrettyPrinter(indent=4)
 	
-	def __init__(self,tr):
-		# window size = 2 * F_VECTOR_LENGTH + 1 
-		F_VECTOR_LENGTH = self.F_VECTOR_LENGTH
-		
-		if(len(tr) < 2*F_VECTOR_LENGTH + 1):
-			raise ValueError("training data too small")
-		
-
-		# remove nan entries
-		tr = tr[~np.isnan(tr).any(axis=1)]
-		# normalize
-		tr[:,1] = scipy.stats.zscore(tr[:,1])
-		
-		# calculate and store the derivative to add to the feature vectors
-		dr = scipy.stats.zscore(np.gradient(tr[:,1]))
-
-		print(tr.shape)
-		tr = np.hstack((tr,dr.reshape((-1,1))))
-		
-		print(tr.shape)
-	
-		# SET UP TRAINING
-		# Maps indicies to blink or not
-		blinkMap = np.array(tr[:,2],copy=True)
-		
-		# Mapping frames to their classification
-		self.tr_vectors = []
-		
-		numblinks = 0
-		
-		# extract blinks 
-		for i in range(F_VECTOR_LENGTH,len(tr)-F_VECTOR_LENGTH):
-			if(tr[i][LABELED_BLINK] == 1):
-				blinkMap[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1] = 1
-				# BALANCING DATA
-				for z in range(100):
-					numblinks += 1
-					self.tr_vectors.append((tr[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1],1))
-				
-		# extract non blinks
-		for i in range(F_VECTOR_LENGTH,len(tr)-F_VECTOR_LENGTH):
-			if(blinkMap[i] == 0):
-				if(1 not in blinkMap[i - F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1]):
-					self.tr_vectors.append((tr[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1],0))
-		
-		# clear memory
-		del blinkMap
-		
-		#print(numblinks)
-		# print(len(self.tr_vectors))
-		
-		#print(self.tr_vectors[0][0])
-		
-		
-		
-	
-			
-	
-	# extracts the labels and features from sample array
-	def get_labels_features(self):
-		labels = [x[1] for x in self.tr_vectors]
-		
-		features = []
-		for feature in [x[0] for x in self.tr_vectors]:
-			features.append([x[1] for x in feature] + [x[3] for x in feature])
-			
-		self.pp.pprint(features[0])
-		
-		return labels,features
-		
-	# train on feature vectors
-	def train(self):
-		labels,features = self.get_labels_features()
-
-		self.internal_model = svm.SVC(gamma='scale')
-		self.internal_model.fit(features,labels)
-		
-	# cross validate
-	def cv(self,fold=10):
-		print("cross validating")
-		
-		labels,features = self.get_labels_features()		
-		
-		model_svm_linear = make_pipeline(svm.SVC(C=1, kernel='linear'))
-		
-		#Metric scores
-		scoring = {'acc': 'accuracy',
-           'prec_macro': 'precision_macro',
-           'rec_macro': 'recall_macro'}
-		  
-		score_lr = cross_validate(model_svm_linear, features, labels, scoring=scoring, cv=fold, return_train_score=True,verbose =True)
-		self.pp.pprint(score_lr)
+	#Metric scores
+	scoring = {'acc': 'accuracy',
+	   'prec_macro': 'precision_macro',
+	   'rec_macro': 'recall_macro'}
 	   
-	# output classification on test data
-	def classify(self,c_vector):  	
-		if(self.internal_model is None):
-			raise ValueError("Need to train model first")
-		
-		F_VECTOR_LENGTH = self.F_VECTOR_LENGTH
-		
-		# extract features
-		c_vectors = []
-		for i in range(F_VECTOR_LENGTH,len(c_vector)-F_VECTOR_LENGTH):
-			c_vectors.append((c_vector[i-F_VECTOR_LENGTH:i+F_VECTOR_LENGTH+1]))	
-		
-		# get ear from test_features
-		c_features = []
-		for feature_list in c_vectors:
-			c_features.append([x[1] for x in feature_list])
+	model_svm_linear = make_pipeline(svm.SVC(C=1, kernel='rbf',gamma='scale',class_weight="balanced"))
 	
-		prediction = self.internal_model.predict(c_features)
-		print(prediction)
-		
-	# save weights
-	def save(self,path):
-		if(self.internal_model is None):
-			raise ValueError("Need to train model first")
-			
-		date = datetime.datetime.today().strftime('%Y_%m_%d')
-		print("saving model to " + path + date)
-		joblib.dump(self.internal_model, path + date + ".joblib") 
+	score_lr = cross_validate(model_svm_linear, X, y, scoring=scoring, cv=5, return_train_score=True)
+	pp.pprint(score_lr)
+	
+def score(model,X,y):
+	y_predict = model.predict(X)
+	print("\ttotal blinks predicted: " + str(np.sum(y_predict)))
+	recall = recall_score(y,y_predict)
+	precision = precision_score(y,y_predict)
+	accuracy = accuracy_score(y,y_predict)
 
-	# load weights 
-	def load(self,path_and_name):
-		print("loading model from " + path_and_name)
-		self.internal_model = joblib.load(path_and_name)
-		
-def test():
-	# test training pre-processing
-	# t1 = np.array([
-		  # [50,1,0],
-		  # [51,1,1],
-		  # [52,1,0],
-		  # [53,1,1],
-		  # [54,1,0],
-		  # # --- 
-		  # [50,1,0],
-		  # [51,1,1],
-		  # [52,1,0],
-		  # [53,1,1],
-		  # [54,1,0],
-		  # # --- 
-		  # [55,1,0],
-		  # [56,1,0],
-		  # [57,0,0],
-		  # [58,0,0],
-		  # [59,0,0],
-		  # # ---
-		  # [55,1,0],
-		  # [56,1,0],
-		  # [57,0,0],
-		  # [58,0,0],
-		  # [59,0,0],
-		  # # --- 
-		  # [60,0,0],
-		  # [61,0,0],
-		  # [62,0,0]])
-		  
-	# test slicing
-	# testSlicing()
-	# svm = SVM(t1)
-	#svm.train()
-	# svm.cv(2)
-	#svm.classify(t1[:,0:2])
-	
-	# current working directory	
-	# cwd = os.path.dirname(os.path.realpath(__file__))
-	# model_path = cwd + "\\saved_models\\"
-	# svm.save(model_path)
-	# svm2 = SVM(t1)
-	# svm2.load("D:\\blink-detection\\saved_models\\2018_11_21.joblib")
-	# svm2.cv(2)
-	
-	# # test training pre-processing
-	# t2 = np.array([
-		  # [50,nan,1],
-		  # [51,1,1],
-		  # [52,1,0],
-		  # [53,1,1],
-		  # [54,nan,0],
-		  # # --- 
-		  # [50,1,0],
-		  # [51,1,1],
-		  # [52,1,1],
-		  # [53,1,1],
-		  # [54,1,1],
-		  # # --- 
-		  # [55,1,0],
-		  # [56,1,0],
-		  # [57,nan,0],
-		  # [58,nan,0],
-		  # [59,nan,0],
-		  # # ---
-		  # [55,1,1],
-		  # [56,1,1],
-		  # [57,0,0],
-		  # [58,0,0],
-		  # [59,0,0],
-		  # # --- 
-		  # [60,0,0],
-		  # [61,0,0],
-		  # [62,0,0]])
-		  
-	# svm2 = SVM(t2)
-	
-	# try training svm on some actual data
-	# pull out data and combine
-	
-	print("loading in ears")
-	txt1 = csvPath + "planesweater1_ears.csv"
-	ears = pd.read_csv(txt1,sep=',',header=None).values
+	print("\trecall: " + str(recall))
+	print("\tPrecision: " + str(precision))
+	print("\tAccuracy: " + str(accuracy))
 
-	print("loading in labels")
-	txt2 = csvPath + "planesweater1_labels.csv"
-	labels = pd.read_csv(txt2,sep=',',header=None).values
-	
-	t3 = np.hstack((ears,labels))
-	svm3 = SVM(t3)
-	svm3.cv(10)
-	
-test()
+	return y_predict
+
+# STEVEN'S DATA ----------------
+TIMESTAMP = 0
+EAR = 0
+GRADIENT = 1
+LABELED_BLINK = 2
+F_VECTOR_LENGTH = 3
+
+print("loading in ears")
+txt1 = csvPath + "1_EAR.csv"
+ears = pd.read_csv(txt1,sep=',',header=None).values[0:18200]
+gradient = np.gradient(ears[:,EAR]).reshape((-1,1))
+ears = np.hstack((ears,gradient))
+
+print("loading in labels")
+txt2 = csvPath + "1_labels.csv"
+labels = pd.read_csv(txt2,sep=',',header=None).values[0:18200]
+raw = np.hstack((ears,labels))
+
+# # MY DATA ------------------
+
+# # print("loading in ears")
+# txt1 = csvPath + "planesweater1_ears.csv"
+# ears = pd.read_csv(txt1,sep=',',header=None).values
+# gradient = np.gradient(ears[:,EAR]).reshape((-1,1))
+# ears = np.hstack((ears,gradient))
+
+# # print("loading in labels")
+# txt2 = csvPath + "planesweater1_labels.csv"
+# labels = pd.read_csv(txt2,sep=',',header=None).values
+# raw = np.hstack((ears,labels))
+
+# remove nan entries
+tr = raw[~np.isnan(raw).any(axis=1)]
+
+# VALIDATE ON EXTRACTED DATA 
+raw_features = extract_features_labels_raw(tr)
+extracted_features = extract_features_labels_true(raw_features,True)
+X,y = X_y(extracted_features)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=1)
+
+
+# RANDOM SEARCH
+C = uniform(loc=1,scale=1000)
+gamma = uniform(loc = 0.001, scale = 0.0001)
+hyperparameters = dict(C=C,gamma=gamma,kernel=['rbf'])
+clf = RandomizedSearchCV(svm.SVC(),hyperparameters,random_state=1,n_iter=100,cv=5,verbose=1)
+best_model = clf.fit(X_train,y_train)
+
+C = best_model.best_estimator_.get_params()['C']
+gamma = best_model.best_estimator_.get_params()['gamma']
+model = svm.SVC(C=C,gamma=gamma,kernel='rbf')
+
+print("training model")
+model.fit(X_train,y_train)
+y_predict = score(model,X_test,y_test)
+
+pd.Series(y_test.flatten()).plot()
+pd.Series(y_predict.flatten()/2).plot()
+plt.show()
+
+# print("cross validating")
+# cross_val(X,y)
+
+# VALIDATE ON RAW DATA
+# raw_features = extract_features_labels_raw(tr)
+# X_raw,y_raw = X_y(raw_features)
+# extracted = extract_features_labels_true(raw_features,True)
+
+# X_train_raw, X_test_raw, y_train_raw, y_test_raw = train_test_split(X_raw,y_raw,test_size=0.20,random_state=49)
+
+# X_train,y_train = X_y(extract_features_labels_true(np.hstack((X_train_raw,y_train_raw.reshape((-1,1))))))
+# model = svm.SVC(gamma='scale',kernel='rbf')
+# model.fit(X_train,y_train)
+
+# y_predict = score(model,X_test_raw,y_test_raw)
+# pd.Series(y_predict.flatten()/2).plot()
+# pd.Series(y_test_raw.flatten()).plot()
+# plt.show()
+
+# # extracted_features = extract_features_labels_true(raw_features,False)
+# # X,y = X_y(extracted_features)
+# # X,y = X_y(under_sample_balance(extracted_data))
+# # print("cross validating")
+# # cross_val(X,y)
+
+# # model = svm.SVC(gamma='scale')
+# # model.fit(X,y)
+# # extracted_data_raw = extract_features_labels_raw(test,True)
+# # X,y = X_y(extracted_data_raw)
+# # y_predict = score(model,X,y)
+
+# # pd.Series(y_predict.flatten()).plot()
+# # pd.Series(y.flatten()).plot()
+# # plt.show()
+
